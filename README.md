fa0658cff145500ac690982efbf2f0e8eed7d06e
Luisoteko <luisoteko100@gmail.com>
2018-07-16 00:00:40 -0500

    Initial Commit && Completed Project

---
+++ README.md
@@ -0,0 +1,26 @@
+# Curso de PWA con Recat en Platzi
+
+![Captura de Recetas](.readme-static/captura.png)
+PWA que utiliza la API de MealDB para mostrar recetas con sus instrucciones. Algunas de las features que tiene son:
+* Soporte Offline con Workbox
+* Estrategias de Red apropiadas para cada recurso
+* Add to Home Screen
+* Web Share API
+
+[Ver la app en Produccion](https://platzi-recetas-luisoteko.now.sh/)
+
+## Scripts
+
+* `npm install` para instalar dependencias
+* `npm run dev` para entorno de desarrollo
+* `npr run prod` para produccion
+
+## Recordatorios
+
+* Service Workers solo funcionan en produccion
+* La mayoria de las caracteristicas solo funcionan en un servidor HTTPS
+
+## Licencia
+MIT
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app)
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+// btnAdd.addEventListener('click', (e) => {
+//   // hide our user interface that shows our A2HS button
+//   btnAdd.style.display = 'none';
+//   // Show the prompt
+//   deferredPrompt.prompt();
+//   // Wait for the user to respond to the prompt
+//   deferredPrompt.userChoice
+//     .then((choiceResult) => {
+//       if (choiceResult.outcome === 'accepted') {
+//         console.log('User accepted the A2HS prompt');
+//       } else {
+//         console.log('User dismissed the A2HS prompt');
+//       }
+//       deferredPrompt = null;
+//     });
+// });
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
